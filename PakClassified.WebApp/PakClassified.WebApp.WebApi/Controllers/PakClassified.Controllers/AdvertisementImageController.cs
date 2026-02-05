e100f125bf1bce8a2c2dd18c8aa2fa5537a2da7f
Muhammad Abuzar Chughtai <chughtaimabuzar@gmail.com>
2026-02-06 00:32:12 +0500

    Apis Completed

--- PakClassified.WebApp/PakClassified.WebApp.WebApi/Controllers/PakClassified.Controllers/AdvertisementImageController.cs
+++ PakClassified.WebApp/PakClassified.WebApp.WebApi/Controllers/PakClassified.Controllers/AdvertisementImageController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using b._PakClassified.WebApp.Services.Enitities.Services.PakClassified.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PakClassified.WebApp.DTOs.PakClassified.DTOs;
+using System.Security.Claims;
 
 namespace PakClassified.WebApp.WebApi.Controllers.PakClassified.Controllers
 {
@@ -7,5 +10,115 @@ namespace PakClassified.WebApp.WebApi.Controllers.PakClassified.Controllers
     [ApiController]
     public class AdvertisementImageController : ControllerBase
     {
+        private readonly IAdvertisementImageService _advertisementImageService;
+        private readonly ILogger<AdvertisementImageController> _logger;
+
+        public AdvertisementImageController(IAdvertisementImageService advertisementImageService, ILogger<AdvertisementImageController> logger)
+        {
+            _advertisementImageService = advertisementImageService;
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        [Route("GetAll/Advertisement/{id}")]
+        public async Task<IActionResult> GetAllAdvertisementImages(int id)
+        {
+            _logger.LogInformation("Fetching all Advertisement Images.");
+
+            var response = (await _advertisementImageService.GetAllAsync(id));
+
+            if (response.Count() <= 0)
+            {
+                _logger.LogWarning("No Images for this advertisement Found in Database.");
+                return NotFound(new { message = "No Image for Advertisement Found." });
+            }
+
+            _logger.LogInformation("Successfully retrived {Count} Advertisement Images", response.Count());
+            return Ok(response);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            _logger.LogInformation("Fetching Advertisement Image by Id: \"{id}\"", id);
+
+            var response = await _advertisementImageService.GetByIdAsync(id);
+            if (response == null)
+            {
+                _logger.LogWarning("No Advertisement Image was found by this Id: \"{id}\"", id);
+                return NotFound(new { message = $"No Advertisement Image was found by this id {id}" });
+            }
+
+            _logger.LogInformation("Successfully retrive Advertisement Image: {name} with id: {id}", response.Name, response.Id);
+            return Ok(response);
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<IActionResult> Create([FromBody] AdvertisementImageModel request)
+        {
+
+            // IFormFile → Byte[]
+
+
+            _logger.LogInformation("Creating Advertisement Image: {Name}", request.Name);
+
+            string LoginUser = User.FindFirstValue(ClaimTypes.Name);     // Login User, Extracted from Token
+            request.CreatedBy = LoginUser;
+
+            var response = (await _advertisementImageService.CreateAsync(request));
+
+            _logger.LogInformation("Successfully Created a Advertisement Image {Name}", response.Name);
+
+            return Created(string.Empty, response);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Update([FromBody] AdvertisementImageModel request, int id)
+        {
+
+            // IFormFile → Byte[]
+
+
+
+            _logger.LogInformation("Fetching the Advertisement Image ({id}) to Update.", id);
+
+            string LoginUser = User.FindFirstValue(ClaimTypes.Name); // Login User, Extracted from Token
+            request.LastModifiedBy = LoginUser;
+
+            var response = await _advertisementImageService.UpdateAsync(id, request);
+
+            if (response == null)
+            {
+                _logger.LogWarning("Failed to Fetch the Advertisement Image ({id}).", id);
+                return NotFound(new { message = $"No Advertisement Image was found with Id: {id}" });
+            }
+
+            _logger.LogInformation("Successfully Updated Advertisement Image with Id: {id}", response.Id);
+            return Ok(response);
+        }
+
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            _logger.LogInformation("Fetching the Advertisement Image ({id}) to Delete.", id);
+
+            string LoginUser = User.FindFirstValue(ClaimTypes.Name);          // Login User, Extracted from Token
+
+            var response = await _advertisementImageService.DeleteAsync(id, LoginUser);
+
+            if (response == null)
+            {
+                _logger.LogWarning("Failed to Fetch the Advertisement Image ({id}).", id);
+                return NotFound(new { message = $"No Advertisement Image was found with Id: {id}" });
+            }
+            _logger.LogInformation("Successfully Deleted Advertisement Image with Id: {id}", response.Id);
+            return Ok(response);
+        }
     }
 }
--- PakClassified.WebApp/PakClassified.WebApp.WebApi/Controllers/PakClassified.Controllers/AdvertisementSubCategoryController.cs
+++ PakClassified.WebApp/PakClassified.WebApp.WebApi/Controllers/PakClassified.Controllers/AdvertisementSubCategoryController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using b._PakClassified.WebApp.Services.Enitities.Services.PakClassified.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PakClassified.WebApp.DTOs.PakClassified.DTOs;
+using System.Security.Claims;
 
 namespace PakClassified.WebApp.WebApi.Controllers.PakClassified.Controllers
 {
@@ -7,5 +10,106 @@ namespace PakClassified.WebApp.WebApi.Controllers.PakClassified.Controllers
     [ApiController]
     public class AdvertisementSubCategoryController : ControllerBase
     {
+        private readonly IAdvertisementSubCategoryService _advertisementSubCategoryService;
+        private readonly ILogger<AdvertisementSubCategoryController> _logger;
+
+        public AdvertisementSubCategoryController(IAdvertisementSubCategoryService advertisementSubCategoryService, ILogger<AdvertisementSubCategoryController> logger)
+        {
+            _advertisementSubCategoryService = advertisementSubCategoryService;
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> GetAllAdvertisementCategories()
+        {
+            _logger.LogInformation("Fetching all Advertisement Categories.");
+
+            var response = await _advertisementSubCategoryService.GetAllAsync();
+
+            if (response.Count() <= 0)
+            {
+                _logger.LogWarning("No Advertisement Sub Category Found in Database.");
+                return NotFound(new { message = "No Advertisement Sub Category Found." });
+            }
+
+            _logger.LogInformation("Successfully retrived {Count} Advertisements Sub Category", response.Count());
+            return Ok(response);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            _logger.LogInformation("Fetching Advertisement Sub Category by Id: \"{id}\"", id);
+
+            var response = await _advertisementSubCategoryService.GetByIdAsync(id);
+            if (response == null)
+            {
+                _logger.LogWarning("No Advertisement Sub Category was found by this Id: \"{id}\"", id);
+                return NotFound(new { message = $"No Advertisement Sub Category was found by this id {id}" });
+            }
+
+            _logger.LogInformation("Successfully retrive Advertisement Sub Category: {name} with id: {id}", response.Name, response.Id);
+            return Ok(response);
+        }
+
+        [HttpPost]
+        [Route("")]
+        public async Task<IActionResult> Create([FromBody] AdvertisementSubCategoryModel request)
+        {
+            _logger.LogInformation("Creating Advertisement Sub Category: {Name}", request.Name);
+
+            string LoginUser = User.FindFirstValue(ClaimTypes.Name);     // Login User, Extracted from Token
+            request.CreatedBy = LoginUser;
+
+            var response = (await _advertisementSubCategoryService.CreateAsync(request));
+
+            _logger.LogInformation("Successfully Created a Advertisement Sub Category {Name}", response.Name);
+
+            return Created(string.Empty, response);
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Update([FromBody] AdvertisementSubCategoryModel request, int id)
+        {
+            _logger.LogInformation("Fetching the Advertisement Sub Category ({id}) to Update.", id);
+
+            string LoginUser = User.FindFirstValue(ClaimTypes.Name); // Login User, Extracted from Token
+            request.LastModifiedBy = LoginUser;
+
+            var response = await _advertisementSubCategoryService.UpdateAsync(id, request);
+
+            if (response == null)
+            {
+                _logger.LogWarning("Failed to Fetch the Advertisement Sub Category ({id}).", id);
+                return NotFound(new { message = $"No Advertisement Sub Category was found with Id: {id}" });
+            }
+
+            _logger.LogInformation("Successfully Updated Advertisement Sub Category with Id: {id}", response.Id);
+            return Ok(response);
+        }
+
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            _logger.LogInformation("Fetching the Advertisement Sub Category ({id}) to Delete.", id);
+
+            string LoginUser = User.FindFirstValue(ClaimTypes.Name);          // Login User, Extracted from Token
+
+            var response = await _advertisementSubCategoryService.DeleteAsync(id, LoginUser);
+
+            if (response == null)
+            {
+                _logger.LogWarning("Failed to Fetch the Advertisement Sub Category ({id}).", id);
+                return NotFound(new { message = $"No Advertisement Sub Category was found with Id: {id}" });
+            }
+            _logger.LogInformation("Successfully Deleted Advertisement Sub Category with Id: {id}", response.Id);
+            return Ok(response);
+        }
     }
 }
